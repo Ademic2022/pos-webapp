c6e9d0baeb50531ade83855b26f3151a5652fde6
Adeeyo Michael <adeeyomichael77@gmail.com>
2025-06-09 22:49:07 +0100

    Update user.ts

--- src/data/user.ts
+++ src/data/user.ts
@@ -7,7 +7,7 @@ export const loggedInUser: User = {
   username: "adaokafor",
   phone: "+2348012345678",
   is_staff: true,
-  is_superuser: false,
+  is_superuser: true,
   createdAt: "2024-11-10T08:23:00Z",
   isActive: true,
 };
